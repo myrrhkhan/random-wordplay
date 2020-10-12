228d6340e79d114460f81a0b2dcc899535c34b43
myrrhkhan <57461851+myrrhkhan@users.noreply.github.com>
2020-10-12 20:31:56 +0000

    updated file

--- name.py
+++ name.py
@@ -1,11 +1,12 @@
+import random
 def load_words():
-    with open('words_alpha.txt') as word_file:
+    file = 'english-words\\words.txt'
+    with open(file) as word_file:
         valid_words = set(word_file.read().split())
 
     return valid_words
 
 
-if __name__ == '__main__':
-    english_words = load_words()
-    # demo print
-    print('fate' in english_words)
+english_words = load_words()
+for i in range(input("input how many times this process should be repeated: ")):
+    print(random(english_words))
