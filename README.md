231af813028abd626b5d08005415b73e238600f4
myrrhkhan <57461851+myrrhkhan@users.noreply.github.com>
2020-10-25 22:06:30 -0400

    Revert "Revert "added some new files, organized the main script into sm""
    This reverts commit e5a37be3747e75fbcf8a1b9a701b771f50956b50.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 Playing around with my friend's last name
+
+Essentially, this takes a bunch of words in the English language, filters them, and prints them out in a way involving some sort of wordplay.
+
+This repo uses another repository with a list of English words.
+
+Unfortunately, profanity_check has a multitude of warnings when imported. It's possible that this library won't be supported for too long.
+
+Nameplay.py is the main script.
+There is a private_vars.py file referenced that establishes a few private variables (i.e. path, etc.). This file is ignored by the gitignore, so the user should create the file themself and establish the variables.
+
+word_writing.py is where the word filtering is configured. The words aren't filtered and written to a filtered.txt file until the nameplay.py file is run and the user agrees to the option of refreshing the filtered.txt file.
--- nameplay.py
+++ nameplay.py
@@ -1,27 +1,26 @@
-import random; import pandas as pd
-def write_words(): #Imports words, puts them through a profanity filter, writes them to filtered words file. Optional
-    from profanity_check import predict #imported here instead of at the top because there are a bunch of warnings when importing :(
-    file = 'english-words\\words_alpha.txt' #Original file.
-    with open(file) as word_file:
-        valid_words = [word for word in list(word_file.read().split()) if word[-2:len(word)] == "er" and predict([word]) == 0]
-        #Takes each word from the original file. Adds them into a list if the last two letters are "er" and if the word is not profane.
-    with open('filtered.txt', 'w+') as filter_file:
-        for word in valid_words: filter_file.write(word + "\n") #Opens the filtered.txt file and writes the list of words into the file.
+import random, pandas as pd, os, private_vars, word_writing
+os.chdir(private_vars.path)
     
 def load_words(): #Loads the words in the filtered words file.
     with open('filtered.txt') as file:
         valid_words = list(file.read().split())
     return valid_words
 
-if input("Would you like to refresh your words list? yes if you do, anything else if not. ") == "yes": write_words() #Asks if you'd like to refresh the words.
+i = int(input("How many words? ")) #put this before refresh so that everyting is just "enter"
+
+if input("Would you like to refresh your words list? yes if you do, anything else if not. ") == "yes": 
+    word_writing.write_words() #Asks if you'd like to refresh the words.
 
 english_words = load_words() #sets the laoded words as a list variable.
-i = int(input("How many words? "))
 list = []; f = 0 #makes a list, starts the counter at 0.
+
 while f < i: #While there aren't enough words
     item = english_words[random.randint(0, len(english_words))] #chooses random word from the list by randomly picking an index.
-    list.append("Kai " + item + "strom"); f += 1 #Adds this to the list.
+    list.append(private_vars.print_mod(item)); f += 1 #Adds this to the list.
+
 print(list) #Prints list for confirmation.
-if input("would you like to copy this as multiple lines or no? ") == "yes" or "": df=pd.DataFrame([list]) #Copy as multiple lines
-else: df=pd.DataFrame(list) #copy as one line
+if input("would you like to copy this as multiple lines or no? ") == "yes" or "": 
+    df=pd.DataFrame([list]) #Copy as multiple lines
+else: 
+    df=pd.DataFrame(list) #copy as one line
 df.to_clipboard(index=False, header=False) #copy to clipboard
